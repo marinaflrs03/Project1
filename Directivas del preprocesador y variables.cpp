194fdf5d95aae51895dfe3051f9cef5e1648603b
marinaflrs03 <marina.flores.zarate@gmail.com>
2022-02-15 20:30:49 -0600

    adding the project

---
+++ Directivas del preprocesador y variables.cpp
@@ -0,0 +1,28 @@
+//Directivas del preprocesador y variables
+
+#include<stdio.h>  //libreria
+
+#define PI 3.1416 //Macro
+
+int y = 5; //variable Global
+
+int main(){
+	int x = 10; //variable Local
+	
+	int suma = 0;
+	
+	suma = PI + x;
+	
+	printf("la suma es: %i",suma);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ Prueba_1.c
@@ -0,0 +1,22 @@
+#include<stdio.h> //libreria
+
+#define PI 3.1416
+
+int y = 5;
+
+int main(){
+	
+	int x = 10; //variable local
+	
+	int suma = 0;
+	
+	suma = PI + x;
+	
+	printf("la suma es: %i",suma);
+	
+	
+	
+	
+	
+	return 0;
+}
